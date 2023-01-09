1c90f830dbdf065e87ee72bb1762427bbdc03bba
Henrique Araújo <henriquemooreira@gmail.com>
2023-01-08 21:05:37 -0300

    add service/ DeleteProduceService fix

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,6 +1,5 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
-import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
 
 interface IRequest {
@@ -11,7 +10,7 @@ class DeleteProductService {
   public async execute({ id }: IRequest): Promise<void> {
     const productsRepository = getCustomRepository(ProductRepository);
 
-    const product = productsRepository.findOne(id);
+    const product = await productsRepository.findOne(id);
 
     if (!product) {
       throw new AppError('Product not found.');

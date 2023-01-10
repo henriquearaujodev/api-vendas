60fd84a2faa34c917f602ce6f6d85bb87d1f2fd2
Henrique Araújo <henriquearaujodev@gmail.com>
2023-01-10 16:33:09 -0300

    add ResetPasswordController

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import ResetPasswordService from '../services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+
+    const resetPassoword = new ResetPasswordService();
+
+    await resetPassoword.execute({ password, token });
+
+    return response.status(204).json();
+  }
+}
